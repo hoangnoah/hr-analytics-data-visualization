28843132e8b7409ae92928beca3011b4cfad806d
hoangnoah <nguyenhoang692004@gmail.com>
2025-05-02 14:09:24 +0700

    fixed

---
+++ database/backup/init.sql
@@ -0,0 +1,41 @@
+CREATE SCHEMA IF NOT EXISTS hr_schema;
+
+CREATE TABLE hr_schema.hr_data (
+    age INT,
+    attrition VARCHAR(10),
+    businesstravel VARCHAR(50),
+    department VARCHAR(100),
+    distancefromhome INT,
+    education INT,
+    educationfield VARCHAR(100),
+    employeenumber INT PRIMARY KEY,
+    environmentsatisfaction INT,
+    gender VARCHAR(10),
+    jobinvolvement INT,
+    joblevel INT,
+    jobrole VARCHAR(100),
+    jobsatisfaction INT,
+    maritalstatus VARCHAR(20),
+    monthlyincome INT,
+    numcompaniesworked INT,
+    over18 CHAR(1),
+    overtime VARCHAR(5),
+    percentsalaryhike INT,
+    performancerating INT,
+    relationshipsatisfaction INT,
+    standardhours INT,
+    stockoptionlevel INT,
+    totalworkingyears INT,
+    trainingtimeslastyear INT,
+    worklifebalance INT,
+    yearsatcompany INT,
+    yearsincurrentrole INT,
+    yearssincelastpromotion INT,
+    yearswithcurrmanager INT,
+    date_of_resignation TIMESTAMP
+);
+
+COPY hr_schema.hr_data
+FROM '/dataset/hr_employee_attrition.csv'
+DELIMITER ','
+CSV HEADER;
